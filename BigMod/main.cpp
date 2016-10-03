155cd84db2d0b9a44774d621c2e563e26a4db8ff
Robin Andersson <paul.robin.andersson@gmail.com>
2016-10-03 15:53:39 +0200

    Big Mod complete

--- BigMod/main.cpp
+++ BigMod/main.cpp
@@ -1,28 +1,40 @@
 // Robin Andersson, ae5929, paul.robin.andersson@gmail.com
-// 10340
+// 374
+// http://stackoverflow.com/questions/13553057/using-powx-y-in-large-numbers
+// https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/modular-exponentiation
 #include <iostream>
 
 using namespace std;
 
+int B = 0;
+int P = 0;
+int M = 0;
+
+int mymod(int B, int P, int M)
+{
+	// If something is power of 0 it's 1
+	if (P == 0)
+		return 1;
+
+	// If P is not an even number
+	if (P % 2 != 0)
+	{
+		return (B % M * mymod(B, P - 1, M)) % M;
+	}
+	else
+	{
+		// if P is even we can safley divide by 2
+		int j = mymod(B, P / 2, M);
+		return ((j % M) * (j % M)) % M;
+	}
+}
+
 int main()
 {
-	while (!cin.eof())
+	while (cin >> B >> P >> M)
 	{
-		queue<char> f;
-		cin >> first >> second;
-
-		for (int i = 0; i < first.length(); ++i)
-			f.push(first[i]);
-
-		for (int i = 0; i < second.length(); ++i)
-		{
-			if (f.size() != 0 && f.front() == second[i])
-				f.pop();
-		}
-
-		cout << (f.empty() ? "Yes" : "No");
-		if (!cin.eof())
-			cout << endl;
+		float result = mymod(B, P, M);
+		cout << result << endl;
 	}
 
 	return 0;
