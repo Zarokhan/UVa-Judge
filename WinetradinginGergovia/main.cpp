5c0c18039b0f07177d6995ea3255d9f93c1b8064
Robin Andersson <paul.robin.andersson@gmail.com>
2016-10-06 19:17:11 +0200

    Wine trading in Gergovia Complete

--- WinetradinginGergovia/main.cpp
+++ WinetradinginGergovia/main.cpp
@@ -2,56 +2,59 @@
 // 11054
 // http://www.algolist.net/Algorithms/Sorting/Quicksort
 #include <iostream>
+#include <cmath>
 #define MAX 100000
 
 using namespace std;
 
-struct Node
-{
-	Node(int _i, int _w) : i(_i), w(_w) {}
-	Node(const Node& n) : i(n.i), w(n.w) { }
-	Node() { i = 0; w = 0; }
+//struct Node
+//{
+//	Node(int _i, int _w) : i(_i), w(_w) {}
+//	Node(const Node& n) : i(n.i), w(n.w) { }
+//	Node() { i = 0; w = 0; }
+//
+//	int i;	// index
+//	int w;	// wine
+//
+//	bool operator < (const Node& a) const { return this->w < a.w; }
+//	bool operator > (const Node& b) const { return this->w > b.w; }
+//};
 
-	int i;	// index
-	int w;	// wine
+//void QuickSort(Node arr[], int left, int right)
+//{
+//	int i = left, j = right;
+//	const Node& pivot = arr[(left + right) / 2];
+//
+//	while (i <= j)
+//	{
+//		while (arr[i] < pivot)
+//			++i;
+//		while (arr[j] > pivot)
+//			--j;
+//		
+//		if (i <= j)
+//		{
+//			Node tmp(arr[i]);
+//			arr[i] = arr[j];
+//			arr[j] = tmp;
+//			++i;
+//			--j;
+//		}
+//	}
+//
+//	if (left < j)
+//		QuickSort(arr, left, j);
+//	if (i < right)
+//		QuickSort(arr, i, right);
+//}
 
-	bool operator < (const Node& a) const { return this->w < a.w; }
-	bool operator > (const Node& b) const { return this->w > b.w; }
-};
-
-void QuickSort(Node arr[], int left, int right)
-{
-	int i = left, j = right;
-	const Node& pivot = arr[(left + right) / 2];
-
-	while (i <= j)
-	{
-		while (arr[i] < pivot)
-			++i;
-		while (arr[j] > pivot)
-			--j;
-		
-		if (i <= j)
-		{
-			Node tmp(arr[i]);
-			arr[i] = arr[j];
-			arr[j] = tmp;
-			++i;
-			--j;
-		}
-	}
-
-	if (left < j)
-		QuickSort(arr, left, j);
-	if (i < right)
-		QuickSort(arr, i, right);
-}
-
-Node n[MAX];
+//Node n[MAX];
+long long n[MAX];
 
 int main()
 {
-	int size = 0;
+	long long size = 0;
+	long long total = 0;
 
 	while (true)
 	{
@@ -59,14 +62,23 @@ int main()
 		if (size == 0)
 			return 0;
 
-		for (int i = 0; i < size; ++i)
+		for (long long i = 0; i < size; ++i)
 		{
-			int w = 0;
+			long long w = 0;
 			cin >> w;
-			n[i] = Node(i, w);
+			n[i] = w;
 		}
 
+		long long& temp = n[0];
+		for (long long i = 1; i < size; ++i)
+		{
+			total += abs(temp);
+			n[i] += temp;
+			temp = n[i];
+		}
 
+		cout << total << endl;
+		total = 0;
 	}
 
 	return 0;
