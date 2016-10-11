bc426a66fcfabc922ebd70cdc740c94075e0800b
Robin Andersson <paul.robin.andersson@gmail.com>
2016-10-11 19:09:27 +0200

    hwo do you add beginning

---
+++ Howdoyouadd/main.cpp
@@ -0,0 +1,26 @@
+// Robin Andersson, AE5929, TGSPA14h, paul.robin.andersson@gmail.com
+#include <iostream>
+#define KMAX 100
+
+using namespace std;
+
+int K_TABLE[KMAX][KMAX];
+
+int myfunc(int a, int b)
+{
+
+
+	return 0;
+}
+
+int main()
+{
+	int N = 20, K = 2;
+
+
+
+	while (N != 0 && K != 0) {
+		cin >> N >> K;
+
+	}
+}
