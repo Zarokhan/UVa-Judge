d7149c8e8173124a3bd22db324c7f2b06b2ba713
Robin Andersson <paul.robin.andersson@gmail.com>
2016-10-31 22:41:01 +0100

    Unidirectional, some progress...

--- Unidirectional/main.cpp
+++ Unidirectional/main.cpp
@@ -2,9 +2,11 @@
 // 116 - Unidirectional TSP
 #include <iostream>
 #include <queue>
+#include <stack>
 #include <vector>
 #include <algorithm>
 #include <array>
+
 #define MCOLS 10
 #define MROWS 100
 #define BIGNR 1000000
@@ -13,114 +15,71 @@
 
 using namespace std;
 
+struct Node
+{
+	Node(int value, pair<int,int> dim) : v(value), d(dim) { }
+	Node() = default;
+	int v = 0; // value
+	pair<int, int> d; // dim
+	bool discovered = false;
+
+	vector<pair<int, int>> n; // neighbours
+};
+
+struct Path
+{
+	int v = 0; // value of path
+	queue<int> p; // path
+};
+
+struct GNode
+{
+
+};
+
 // Matrix
-int* M[MCOLS];
-vector<int> turns; // output path
+Node* M[MCOLS];
+int rows = 0, cols = 0;
 
-int rows = 0;
-int cols = 0;
-const int getRows() { return rows; }
-
-// Print method
-void printcol(const int* col, const int lenght)
+void DFS(const pair<int,int> v)
 {
-#ifdef DEBUG
-	for (int i = 0; i < lenght; ++i)
-		cout << col[i] << endl;
-	cout << "-" << endl;
-#endif
-}
-
-// Pick smallest element in array
-// c param: index for column
-queue<pair<int, int>> picksmallest(const int rows, const int cols, const int c)
-{
-	// smallest
-	int* column = new int[rows];
-
-	queue<pair<int, int>> good; // pair<value, index>
-	int value = BIGNR;
-	int index = 0;
-	good.push(pair<int, int>(value, index));
-
-	memcpy(column, M[c], sizeof(int) * rows);
-
-	printcol(column, rows);
-
-	// Find valid solution
-	for (int i = 0; i < rows; ++i)
+	stack<pair<int,int>> s;
+	s.push(v);
+	
+	while (!s.empty())
 	{
-		if (column[i] < good.front().first)
+		Node& n = M[s.top().first][s.top().second];
+		s.pop();
+
+		if (!n.discovered)
 		{
-			// Clear
-			queue<pair<int, int>> empty;
-			swap(good, empty);
-			// Push
-			good.push(pair<int, int>(column[i], i));
+			n.discovered = true;
+			pair<int, int> dim = pair<int,int>(n.d);
+
+			if (dim.first < cols)
+			{
+				// Forward
+				dim.first++;
+				s.push(pair<int, int>(dim));
+				n.n.push_back(pair<int, int>(dim));
+
+				// left
+				int valid = dim.second - 1;
+				if (valid < 0)
+					valid = rows - 1;
+
+				s.push(pair<int, int>(dim.first, valid));
+				n.n.push_back(pair<int, int>(dim.first, valid));
+
+				// Right
+				valid = dim.second + 1;
+				if (valid + 1 >= rows)
+					valid = 0;
+
+				s.push(pair<int, int>(dim.first, valid));
+				n.n.push_back(pair<int, int>(dim.first, valid));
+			}
 		}
-		else if (column[i] == good.front().first)
-		{
-			// Push
-			good.push(pair<int, int>(column[i], i));
-		}
-	}
-
-	//sort(column, column + rows);
-
-	free(column);
-	return good;
-}
-
-void GoDownPath(const int column, const int index, int& total) // param 1: current column, param 2: index in column, param 3: total value
-{
-	int* col = new int[rows];
-	vector<pair<int, int>> p; // possible, 1: index, 2: value
-
-	memcpy(col, M[column], sizeof(int) * rows);
-	printcol(col, rows);
-
-	// left
-	int valid = index - 1;
-	if (valid < 0)
-		valid = rows - 1;
-	p.push_back(pair<int, int>(valid, col[valid]));
-
-	// -> forward direction
-	p.push_back(pair<int, int>(index, col[index]));
-
-	// Right
-	valid = index + 1;
-	if (valid + 1 >= rows)
-		valid = 0;
-	p.push_back(pair<int, int>(valid, col[valid]));
-
-#ifdef DEBUG
-	for (int i = 0; i < 3; ++i)
-		cout << p[i].second << endl;
-	cout << "-" << endl;
-#endif
-
-
-
-	// Maybe need list to save two with same value
-	int value = BIGNR;
-	int in = 0;	// index
-	for (int i = 0; i < 3; ++i)
-	{
-		if (p[i].second < value)
-		{
-			in = p[i].first;
-			value = p[i].second;
-		}
-	}
-
-	turns.push_back(in + 1);
-	total += value;
-
-	free(col);
-	if (column < cols)
-	{
-		GoDownPath(column + 1, in, total);
 	}
 }
 
@@ -129,9 +88,9 @@ int main()
 	// Initialize memory
 	for (int i = 0; i < MCOLS; ++i)
 	{
-		M[i] = new int[MROWS];
+		M[i] = new Node[MROWS];
 		for (int j = 0; j < MROWS; ++j)
-			M[i][j] = 0;
+			M[i][j] = Node();
 	}
 
 	while (!cin.eof())
@@ -142,31 +101,15 @@ int main()
 		{
 			for (int c = 0; c < cols; ++c)
 			{
-				cin >> M[c][r];
+				int v;
+				cin >> v;
+				M[c][r] = Node(v, pair<int,int>(c, r));
 			}
 		}
 
-		queue<pair<int, int>> startpoints = picksmallest(rows, cols, 0); // pair: value, index
-		// pick the 3 next valid tiles in next column
-		while (true)//!startpoints.empty())
+		for (int i = 0; i < rows; ++i)
 		{
-			pair<int, int> p = startpoints.front();
-			int total = p.first;
-			int index = p.second;
-			
-			turns.push_back(index + 1);
-			GoDownPath(1, index, total);
-
-			// print 1: path
-			for (auto it = turns.begin(); it != turns.end(); ++it)
-				cout << *it << ' ';
-			cout << endl;
-			// print 2: total
-			cout << total << endl;
-
-			turns.clear();
-			startpoints.pop();
-			break;
+			DFS(pair<int, int>(0, i));
 		}
 	}
 
